5001cabd119533fce1e81c2a08f61660d71b02bb
Noah Gasparro <noahgasparro@Noahs-MacBook-Pro.local>
2020-05-13 01:17:36 -0400

    Version 1.5: Added answer verification and start of puzzle_menu

--- Sudoku_GUI.py
+++ Sudoku_GUI.py
@@ -1,20 +1,20 @@
 import numpy as np
 import random
 import math
-import time
 import gen_puzzle as gen
 from tkinter import *
 import tkinter.font as tkFont
 from tkinter import ttk
-import Pmw
+import puzzle_list as pl
 
 
 if __name__ == "__main__":
 
     # Dimensions of NxN sudoku matrix
-    N = 9
+    N = pl.N
     # The number of nodes to be removed from puzzle
-    K = 50
+    K = pl.K
+
     """
     # creates puzzle with nodes missing and attempts to then solve it
     sudoku_puzzle = gen.Sudoku(N, K)
@@ -55,24 +55,32 @@ if __name__ == "__main__":
         print(np.matrix(final_puzzle))
     """
 
-    def get_value():
-        for i in range(N):
-            for j in range(N):
-                print(entries[i][j].get())
+    puzzle = pl.puzzle
+    solution = pl.solution
 
-    puzzle = [[0, 0, 0, 8, 0, 1, 0, 0, 0],
-              [0, 0, 0, 4, 3, 9, 0, 0, 2],
-              [3, 0, 0, 0, 0, 0, 1, 9, 8],
-              [0, 0, 0, 0, 0, 0, 2, 5, 0],
-              [7, 5, 0, 3, 0, 0, 0, 0, 4],
-              [0, 0, 0, 0, 0, 7, 0, 1, 0],
-              [0, 0, 7, 1, 0, 0, 4, 0, 6],
-              [8, 6, 0, 0, 4, 0, 7, 0, 0],
-              [0, 4, 1, 6, 0, 0, 0, 8, 9]]
-
+    # initializes size, title, and tkinter gui itself
     gui = Tk()
     gui.title("Sparro Sudoku")
-    gui.geometry("1400x800")
+    gui.geometry("1250x800")
+
+    # validates user input and produces appropriate response
+    result_msg = Label(gui)
+    def get_value():
+        global result_msg
+        result_msg.destroy()
+        for i in range(N):
+            for j in range(N):
+                if puzzle[i][j] == 0:
+                    if float(user_solution[i][j].get()) != solution[i][j]:
+                        result_msg = Label(sub_right, text = "Incorrect. Try again", font = sub_font, bg = "red")
+                        result_msg.grid(row=0, column = 0, rowspan = 3)
+                        sub_right.config(bg='red')
+                        return False
+        result_msg = Label(sub_right, text = "You're a genius!", font = sub_font, bg = "green")
+        result_msg.grid(row=0, column = 0, rowspan = 3)
+        sub_right.config(bg='green')
+        return True
+
 
     #font and font size
     title_font = tkFont.Font(family= "Courier", size = 40)
@@ -82,7 +90,7 @@ if __name__ == "__main__":
 
     # title bar for Sudoku puzzle
     title_right = Frame(gui, bg = "blue")
-    title_right_text = Label(title_right, text = "Sparro Sudoku Puzzle", font = title_font, bg = "blue")
+    title_right_text = Label(title_right, text = "Sparro Sudoku", font = title_font, bg = "blue")
     title_right_text.pack()
 
     # menu on left of GUI
@@ -94,11 +102,11 @@ if __name__ == "__main__":
     puzzle_area = Canvas(gui, width = 150, height = 200, background = "blue")
 
     # user solution submission area on right of GUI
-    sub_right = Frame(gui, width = 150, height = 150, bg = "orange")
+    sub_right = Frame(gui, width = 50, height = 150, bg = "orange")
     sub_right_text = Label(sub_right, text = "Sub-Area Here*", font = sub_font, bg = "orange")
     sub_button = Button(sub_right, text = "Submit", height = 2, width = 10, command = get_value)
-    sub_right_text.grid(row=0, column = 2, rowspan = 3, columnspan=3, sticky = 'ne')
-    sub_button.grid(row = 3, column = 2, columnspan = 3, sticky = 'nse')
+    sub_right_text.grid(row=0, column = 0, rowspan = 3, sticky = 'e')
+    sub_button.grid(row = 3, column = 0, sticky = 'se')
     sub_right.rowconfigure(0, weight=1)
 
     # deciding how GUI is formatted
@@ -113,6 +121,10 @@ if __name__ == "__main__":
     gui.grid_columnconfigure(1, weight=2)
     gui.grid_columnconfigure(2, weight=1)
 
+    # creates menu of puzzles that will appear in puzzle area upon click
+    """for i in range(len(pl.puzzle))
+        label = Label(menu_left, text = "Puzzle #"+str(i))"""
+
     # creates NxN grid with gridlines distributed evenly in the canvas window for puzzle
     idx = 0
     while idx < N:
@@ -122,22 +134,18 @@ if __name__ == "__main__":
         ttk.Separator(puzzle_area, orient=VERTICAL).grid(column=0, row=idx, columnspan=N, sticky='sew')
         idx += 1
 
-    """separator = Pmw.PanedWidget(puzzle_area,
-                    orient='vertical',
-                    separatorthickness= 4,
-                    separatorrelief = 'solid')
-    separator.add("try").grid(column=0, row=0, rowspan=N, stick='nse')"""
-
     # inputs puzzle into the canvas window with 0's as available user inputs
-    entries = [ ['' for i in range(N)] for j in range(N)]
+    user_solution = [ ['0' for i in range(N)] for j in range(N)]
     for i in range(N):
         for j in range(N):
             if puzzle[i][j] != 0:
                 label = Label(puzzle_area, text = str(puzzle[i][j]), font = puzzle_font, bg = "blue")
                 label.grid(row=i, column=j)
-            else:
-                entries[i][j] = Entry(puzzle_area, font = puzzle_font, justify = 'center', width = 2, bg = "blue", bd = 2)
-                entries[i][j].grid(row=i, column=j)
+                user_solution[i][j] = label
+            else:           # creates user input for the nodes removed by gen_puzzle.py
+                entry = Entry(puzzle_area, font = puzzle_font, justify = 'center', width = 2, bg = "blue", bd = 2)
+                entry.grid(row=i, column=j)
+                user_solution[i][j] = entry
 
     gui.config(background="black")
     gui.mainloop()
